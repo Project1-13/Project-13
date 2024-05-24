86570ed610f4a36a679b6f7ce3e1dd3f93e052d5
Adam <aorf@femmeosage.net>
2024-05-23 19:01:19 -0500

    working on the event listener for the login button
    added code in js and linked to it in login.html

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 
+const emailInput = document.querySelector('#email-field'); //targeting the email field of the document 
+const passwordInput = document.querySelector('#password-field'); //targeting the password field of the document 
+const loginButton = document.querySelector('#login'); //targeting the login button
+const rememberButton = document.querySelector('#remember') //targeting the remember me button
+
+
+loginButton.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  const email = emailInput.value;
+  const password = passwordInput.value;
+
+  if (email === '') {
+    displayMessage('error', 'Email cannot be blank');
+  } else if (password === '') {
+    displayMessage('error', 'Password cannot be blank');
+  } else {
+    displayMessage('success', 'Registered successfully');
+
+    localStorage.setItem('email', email);
+    localStorage.setItem('password', password);
+   
+  }
+});
