83013bbab19078cd394ac87831ca1eaaf8cd8f81
GooGaru <xwilliam.morrisonx@gmail.com>
2024-05-30 19:54:08 -0400

    added function to set_appointment

---
+++ appointment.js
@@ -0,0 +1,37 @@
+const artistSelection = document.querySelector('#artist-selection');
+const phoneNumberInput = document.querySelector('#phone-number');
+const firstNameInput = document.querySelector('#first-name');
+const lastNameInput = document.querySelector('#last-name');
+const emailInput = document.querySelector('#email-input');
+const messageInput = document.querySelector('#message-input');
+const appointmentDate = document.querySelector('#datepicker');
+
+submitButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    
+    const artist = artistSelection.value;
+    const phoneNumber = phoneNumberInput.value;
+    const firstName = firstNameInput.value;
+    const lastName = lastNameInput.value;
+    const email = emailInput.value;
+    const message = messageInput.value;
+    const date = appointmentDate.value;
+    
+    if (artist === 'Choose Artist') {
+        alert('Please choose an Artist');
+    } if (phoneNumber === '') {
+        alert ('Please enter your phone number');
+    } if (firstName === ''){
+        alert ('Please enter your first name');
+    } if (lastName === '') {
+        alert ('Please enter your last name');
+    }if (email === '') {
+        alert ('Please enter your email');
+    } 
+    
+    
+    const appointmentArray = [artist, phoneNumber, firstName, lastName, email, message, date];
+    console.log(appointmentArray)
+
+    localStorage.setItem('appointmentArray', JSON.stringify(appointmentArray));
+});
